dc4be1afe1f77f8b819269d43c01ff339839fbd7
Nattanon Kh <nattanondev@gmail.com>
2024-03-01 05:07:58 +0700

    update test 9.1

--- sales/system/auth.php
+++ sales/system/auth.php
@@ -5,9 +5,9 @@
     //$request = json_decode(file_get_contents('php://input'));
     //$userId = $request->userId;
 
-        $profile = $db_nms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
+        //$profile = $db_nms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
         //echo json_encode(array('status' => '200'));
-        echo json_encode($profile);
+        //echo json_encode($profile);
         /*
         if($profile['verify'] == '1') {
             $_SESSION['sales_user'] = $profile['id'];
