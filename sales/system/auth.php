79812e0595cdc8f86b4a3b410edfccd6974a931a
Nattanon Kh <nattanondev@gmail.com>
2024-03-01 04:17:24 +0700

    update test 9

--- sales/system/auth.php
+++ sales/system/auth.php
@@ -5,10 +5,9 @@
     $request = json_decode(file_get_contents('php://input'));
     $userId = $request->userId;
 
-    if(!empty($userId)){
         $profile = $db_nms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
-        echo json_encode(array('status' => '200'));
-        //echo json_encode($profile);
+        //echo json_encode(array('status' => '200'));
+        echo json_encode($profile);
         /*
         if($profile['verify'] == '1') {
             $_SESSION['sales_user'] = $profile['id'];
@@ -21,7 +20,7 @@
             unset($_SESSION['pp_permission']);
         }
         */
-    }
+
     //$db->join("tpf_point_trans p", "c.camp_id=p.poit_campaign", "LEFT");
 	//$db->where("p.poit_parent", $profile['memb_id']);
 	//$products = $db->get("tpf_campaign c", null, "c.camp_topic, c.camp_point ,p.poit_datetime");
