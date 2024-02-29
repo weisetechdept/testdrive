1da819dfee2ccf88f195bbdfc7f13f5ed1f962fc
Nattanon Kh <nattanondev@gmail.com>
2024-03-01 05:57:39 +0700

    1.3

--- sales/system/auth.php
+++ sales/system/auth.php
@@ -2,14 +2,12 @@
     session_start();
     require_once '../../db-conn.php';
 
-    $car = $db->get('car');
-    echo json_encode($car);
     //$request = json_decode(file_get_contents('php://input'));
     //$userId = $request->userId;
 
-        //$profile = $db_nms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
+        $profile = $db_nms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
         //echo json_encode(array('status' => '200'));
-        //echo json_encode($profile);
+        echo json_encode($profile);
         /*
         if($profile['verify'] == '1') {
             $_SESSION['sales_user'] = $profile['id'];
