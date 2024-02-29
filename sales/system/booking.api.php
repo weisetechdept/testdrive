6cb7708b0397932f2fa5dfe565d9d614ec6e9123
Nattanon Kh <nattanondev@gmail.com>
2024-03-01 05:30:49 +0700

    update fix

--- sales/system/booking.api.php
+++ sales/system/booking.api.php
@@ -4,10 +4,6 @@
     require_once '../../db-conn.php';
     date_default_timezone_set("Asia/Bangkok");
 
-    if($_SESSION['pp_login'] !== true && $_SESSION['pp_permission'] !== 'user'){
-        header('Location: /404');
-    }
-
     $id = $_SESSION['sales_user'];
 
     if($_GET['get'] == "sales"){
