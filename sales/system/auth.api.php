142fb1e689b656e6203154c45b3346bc64db3427
Nattanon Kh <nattanondev@gmail.com>
2024-03-01 11:19:12 +0700

    fix 0.3

---
+++ sales/system/auth.api.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    require_once '../../db-conn.php';
+    
+    $request = json_decode(file_get_contents('php://input'));
+    $userId = $request->userId;
+
+        $profile = $db_nms->where('line_usrid',$userId)->getOne('db_member');
+        //echo json_encode($profile);
+        if($profile['verify'] == '1') {
+            $_SESSION['pp_login'] = true;
+            $_SESSION['sales_user'] = $profile['id'];
+            $_SESSION['pp_permission'] = $profile['status'];
+            echo json_encode(array('status' => '200', 'permission' => $profile['status']));
+        } else {
+            echo json_encode(array('status' => '400'));
+            unset($_SESSION['pp_login']);
+            unset($_SESSION['sales_user']);
+            unset($_SESSION['pp_permission']);
+        }
+?>
