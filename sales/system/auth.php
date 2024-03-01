d2d9bf92d5feaf72ff3ee16cd9ab94415d5c62d1
Nattanon Kh <nattanondev@gmail.com>
2024-03-01 08:21:40 +0700

    fix v0.2

--- sales/system/auth.php
+++ sales/system/auth.php
@@ -2,9 +2,7 @@
     session_start();
     require_once '../../db-conn.php';
 
-    //$db_nms = new Database('nms');
-
-    $profile = $db_nms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
-    echo json_encode($profile);
+    $dbnms->where('line_usrid','U6f5da61c00cd349634881dafa7a6e624')->getOne('db_member');
+    echo $dbnms->getLastQuery();
       
 ?>
