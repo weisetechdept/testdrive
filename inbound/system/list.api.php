5a1ae49a55f4bb13f2535b37918399102f9b7e9a
Nattanon Kh <weisetechinfo@gmail.com>
2024-12-26 16:27:38 +0700

    update time ranges in customTime functions for accuracy

--- inbound/system/list.api.php
+++ inbound/system/list.api.php
@@ -35,25 +35,48 @@
         
     function customTime($time){
         if($time == '1'){
-            return '08:00 - 08:45';
+            return '08:00 - 08:30';
         }elseif($time == '2'){
-            return '09:00 - 09:45';
+            return '08:31 - 09:00';
         }elseif($time == '3'){
-            return '10:00 - 10:45';
+            return '09:01 - 09:30';
         }elseif($time == '4'){
-            return '11:00 - 11:45';
+            return '09:31 - 10:00';
         }elseif($time == '5'){
-            return '12:00 - 12:45';
+            return '10:01 - 10:30';
         }elseif($time == '6'){
-            return '13:00 - 13:45';
+            return '10:31 - 11:00';
         }elseif($time == '7'){
-            return '14:00 - 14:45';
+            return '11:01 - 11:30';
         }elseif($time == '8'){
-            return '15:00 - 15:45';
+            return '11:31 - 12:00';
         }elseif($time == '9'){
-            return '16:00 - 16:45';
+            return '12:01 - 12:30';
+        }elseif($time == '10'){
+            return '12:31 - 13:00';
+        }elseif($time == '11'){
+            return '13:01 - 13:30';
+        }elseif($time == '12'){
+            return '13:31 - 14:00';
+        }elseif($time == '13'){
+            return '14:01 - 14:30';
+        }elseif($time == '14'){
+            return '14:31 - 15:00';
+        }elseif($time == '15'){
+            return '15:01 - 15:30';
+        }elseif($time == '16'){
+            return '15:31 - 16:00';
+        }elseif($time == '17'){
+            return '16:01 - 16:30';
+        }elseif($time == '18'){
+            return '16:31 - 17:00';
+        }elseif($time == '19'){
+            return '17:01 - 17:30';
+        }elseif($time == '20'){
+            return '17:31 - 18:00';
         }
-    } 
+
+    }
 
     function customTime2($time){
 
@@ -65,41 +88,85 @@
         if($first == '1'){
             $first = '08:00';
         } elseif($first == '2'){
-            $first = '09:00';
+            $first = '08:31';
         } elseif($first == '3'){
-            $first = '10:00';
+            $first = '09:01';
         } elseif($first == '4'){
-            $first = '11:00';
+            $first = '09:31';
         } elseif($first == '5'){
-            $first = '12:00';
+            $first = '10:01';
         } elseif($first == '6'){
-            $first = '13:00';
+            $first = '10:31';
         } elseif($first == '7'){
-            $first = '14:00';
+            $first = '11:01';
         } elseif($first == '8'){
-            $first = '15:00';
+            $first = '11:31';
         } elseif($first == '9'){
-            $first = '16:00';
+            $first = '12:01';
+        } elseif($first == '10'){
+            $first = '12:31';
+        } elseif($first == '11'){
+            $first = '13:01';
+        } elseif($first == '12'){
+            $first = '13:31';
+        } elseif($first == '13'){
+            $first = '14:01';
+        } elseif($first == '14'){
+            $first = '14:31';
+        } elseif($first == '15'){
+            $first = '15:01';
+        } elseif($first == '16'){
+            $first = '15:31';
+        } elseif($first == '17'){
+            $first = '16:01';
+        } elseif($first == '18'){
+            $first = '16:31';
+        } elseif($first == '19'){
+            $first = '17:01';
+        } elseif($first == '20'){
+            $first = '17:31';
         }
 
         if($last == '1'){
-            $last = '08:45';
+            $last = '08:30';
         } elseif($last == '2'){
-            $last = '09:45';
+            $last = '09:00';
         } elseif($last == '3'){
-            $last = '10:45';
+            $last = '09:30';
         } elseif($last == '4'){
-            $last = '11:45';
+            $last = '10:00';
         } elseif($last == '5'){
-            $last = '12:45';
+            $last = '10:30';
         } elseif($last == '6'){
-            $last = '13:45';
+            $last = '11:00';
         } elseif($last == '7'){
-            $last = '14:45';
+            $last = '11:30';
         } elseif($last == '8'){
-            $last = '15:45';
+            $last = '12:00';
         } elseif($last == '9'){
-            $last = '16:45';
+            $last = '12:30';
+        } elseif($last == '10'){
+            $last = '13:00';
+        } elseif($last == '11'){
+            $last = '13:30';
+        } elseif($last == '12'){
+            $last = '14:00';
+        } elseif($last == '13'){
+            $last = '14:30';
+        } elseif($last == '14'){
+            $last = '15:00';
+        } elseif($last == '15'){
+            $last = '15:30';
+        } elseif($last == '16'){
+            $last = '16:00';
+        } elseif($last == '17'){
+            $last = '16:30';
+        } elseif($last == '18'){
+            $last = '17:00';
+        } elseif($last == '19'){
+            $last = '17:30';
+        } elseif($last == '20'){
+            $last = '18:00';
         }
 
         return $first.' - '.$last;
